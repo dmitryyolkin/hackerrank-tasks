f7db4a13da63a97ea8efc329623bbc27a4895bad
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2021-01-28 10:03:45 +0400

    Task: Array Manipulation (Performance improvements)

--- src/main/java/sandbox/hackerrank/arrays/SolutionArrayManipulation.java
+++ src/main/java/sandbox/hackerrank/arrays/SolutionArrayManipulation.java
@@ -11,6 +11,37 @@ public class SolutionArrayManipulation {
 
     // Result: 200
     public long calculate(int n, int[][] queries) {
+        // Solution
+        // 1     2     3        4     5
+        // 0     0     0        0     0     - start
+        // 100   0     -100     0     0     - 1st
+        // 100   100   -100     0     0     - 2nd
+        // 100   100   0        0     -100  - 3rd
+        long[] arr = new long[n];
+        for (int[] row : queries) {
+            int startIndex = row[0];
+            int endIndex = row[1];
+            int value = row[2];
+
+            arr[startIndex - 1] += value;
+            if (endIndex < n) {
+                // inclusive
+                arr[endIndex] -= value;
+            }
+        }
+
+        long max = 0;
+        long x = 0;
+        for (long value : arr) {
+            x += value;
+            if (max < x) {
+                max = x;
+            }
+        }
+        return max;
+    }
+
+    public long calculateUsual(int n, int[][] queries) {
         long[] arr = new long[n];
         long max = 0;
         for (int[] row : queries) {
