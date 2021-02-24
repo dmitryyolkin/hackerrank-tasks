45872fec5257298b67578dbfa7fd8afd4aaaf89b
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2021-02-24 17:20:25 +0400

    Task: Speed up improvements

--- src/main/java/sandbox/hackerrank/sorting/SolutionSpecialString.java
+++ src/main/java/sandbox/hackerrank/sorting/SolutionSpecialString.java
@@ -6,39 +6,59 @@ public class SolutionSpecialString {
     // Example
     // abcbaba
     // a, b, c, b, a, b, a, bcb, bab, aba,
-    public int calculate(int n, String s) {
-        int result = s.length();
+    public long calculate(int n, String s) {
+        // initialize counter to n because each character is a
+        // palindromic string
+        int counter = n;
 
-        for (int i = 2; i <= s.length(); i++) { // iter by groups (2, 3, .., length)
-            //calculate combinations
-            int medianIndex = i / 2;
-            for (int j = 0; j < s.length(); j++) { // iter by letters
-                char ch = s.charAt(j);
-                if (j + i > s.length()) {
-                    // no options
-                    break;
-                }
+        // to count consecutive characters that are the same
+        int consec = 1;
 
-                boolean valid = true;
-                for (int k = 1; k < i; k++) { // build a group of a certain langth
-                    char c = s.charAt(j + k);
-                    if (i % 2 == 1 && k == medianIndex) {
-                        // odd letters - in the middle can be another letter
-                        // ignore it
-                    } else if (c != ch) {
-                        valid = false;
-                        break;
+        // the middle index of a 3-character symmetry,
+        // assigned only once detected
+        int midIndex = -1;
+
+        // compare with previous character so start with i=1
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i-1)) {
+                // Condition 1: All of the characters are the same
+                // For n consecutive characters that are the same,
+                // we have this formula:
+                // Number of palindromic strings =
+                //     (n-1) + (n-2) + ... + (n-(n-1))
+                counter += consec;
+                consec++;
+
+                // Condition 2: All characters except the middle one
+                // are the same
+                if (midIndex > 0) {
+                    // check for symmetry on both sides
+                    // of the midIndex
+                    if ((midIndex-consec) > 0 && s.charAt(midIndex-consec) == s.charAt(i)) {
+                        counter++;
+                    } else {
+                        // no more possibility of palindromic string
+                        // with this midIndex
+                        midIndex = -1;
                     }
                 }
+            } else {
+                // reset consecutive chars counter to 1
+                consec = 1;
 
-                if (valid) {
-                    result++;
+                // check for a 3-character symmetry
+                if (((i-2) >= 0) && s.charAt(i-2) == s.charAt(i)) {
+                    counter++; // 3-char symmetry is detected
+
+                    // to check if the next characters are the same
+                    // and symmetrical along the midIndex
+                    midIndex = i-1;
+                } else {
+                    midIndex = -1;
                 }
-
             }
-
         }
-        return result;
+        return counter;
     }
 
 }
