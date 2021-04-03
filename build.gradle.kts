1c163d8371c6825ed08e25ea2da5bea2923d463e
Dmitry Yolkin <dmitry.yolkin@maxifier.com>
2021-04-03 11:05:57 +0400

    Set explicit Java 1.8 compability

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,15 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+java {
+    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 }
